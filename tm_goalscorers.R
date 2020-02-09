8693fd7baf66bff0e471696df2fbfed715f00a05
Gustavo-CR <gustavo.acrz@gmail.com>
2020-02-08 20:18:18 -0600

    Visualization design

--- tm_goalscorers.R
+++ tm_goalscorers.R
@@ -1,6 +1,7 @@
 ## Load packages ====
 library(tidyverse)
 library(rvest)
+library(ggrepel)
 
 ## Webpage with goals and assists numbers ====
 tm_url <- "https://www.transfermarkt.co.in/laliga/scorerliste/wettbewerb/ES1/saison_id/2019/altersklasse/alle/plus/1/page/"
@@ -58,22 +59,65 @@ rvest_tm_squads <- function(name, id) {
 squads <- map2_dfr(team_names, team_ids, rvest_tm_squads, .id = "team")
 
 ## Compute proportions by team ====
-scorers %>% 
+scorers_prop <- scorers %>% 
   left_join(select(squads, player_name, team), c("player" = "player_name")) %>% 
   mutate(team = if_else(player == "Karl Toko Ekambi", "fc-villarreal", team)) %>% 
   group_by(team) %>% 
   add_tally(goals, name = "team_goals") %>% 
   add_tally(assists, name = "team_assists") %>% 
-  mutate(goal_prop = goals / team_goals, 
-         assist_prop = assists / team_assists) %>% 
-  drop_na(team) %>% 
-  filter(goal_prop > .25 | assist_prop > .15) %>% 
-  ggplot() + 
-  aes(goal_prop, assist_prop, label = player) + 
-  geom_point() + 
-  geom_text() + 
-  geom_hline(yintercept = .15) + 
-  geom_vline(xintercept = .25) + 
-  theme_minimal()
+  mutate(goal_prop = goals / team_goals * 100, 
+         assist_prop = assists / team_assists * 100) %>% 
+  drop_na(team)
 
+## Build new position variable ====
+scorers_prop <- scorers_prop %>% 
+  mutate(
+    position2 = case_when(
+      str_detect(position, "Goalkeeper") ~ "Portero", 
+      str_detect(position, "Back") ~ "Defensa", 
+      str_detect(position, "Midfield") ~ "Mediocampista", 
+      str_detect(position, "Forward|Striker|Winger") ~ "Delantero"
+    )
+  )
+
+## Save temporary dataset ====
+### (Preserve a copy in case getting data online isn't available)
+save(scorers_prop, file = "tmp/scorers_prop.Rdata")
+
+## Set vis colors ====
+background <- "#0D0C2B"
+lines <- "ivory"
+text <- "whitesmoke"
+goalkeepers <- "grey"
+defenders <- "springgreen"
+midfielders <- "orange"
+strikers <- "firebrick1"
+
+## Set vis font ====
+windowsFonts(SegoeUI = windowsFont("Segoe UI"))
+windowsFonts(TrebuchetMS = windowsFont("Trebuchet MS"))
+
+## Visualize results ====
+ggplot(scorers_prop) + 
+  aes(goal_prop, assist_prop, label = player, color = position2) + 
+  geom_hline(yintercept = 18, color = lines, alpha = .1, size = 1.5) + 
+  geom_vline(xintercept = 22, color = lines, alpha = .1, size = 1.5) + 
+  annotate("text", x = 40, y = 35, 
+           family = "SegoeUI", fontface = "bold.italic", color = text, 
+           label = "Alta participación \nen goles y asistencias") + 
+  geom_point(alpha = .5) + 
+  geom_text_repel(data = filter(scorers_prop, goal_prop > 24 | assist_prop > 20), 
+    size = 3.8, fontface = "bold.italic", family = "SegoeUI") + 
+  scale_color_manual(values = c(defenders, strikers, midfielders, goalkeepers)) + 
+  labs(x = "Porcentaje de goles", y = "Porcentaje de asistencias", 
+       caption = "Fuente: TransferMarkt") + 
+  theme_minimal() + 
+  theme(panel.grid = element_blank(), 
+        plot.background = element_rect(fill = background), 
+        text = element_text(family = "SegoeUI", color = text), 
+        axis.text = element_text(color = text), 
+        legend.position = "none")
+
+## Save visualization png ====
+ggsave("tmp/goals_assists.png", width = 40, height = 20, units = "cm")
 
